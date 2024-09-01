d253c87bacf1fc6d251068d3eb8ac5ef19e7c66c
rudra <rudrakhandelwal1212@gmail.com>
2024-09-01 22:48:05 +0530

    add linked list in dsa

---
+++ Linked List/creationDisaplay.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+}*head, *temp = NULL;
+
+void createNode(){
+     char ch;
+    do
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        printf("Enter data:\n");
+        scanf("%d", &newNode->data);
+        newNode ->next = NULL;
+
+        if(head == NULL){
+            head = newNode;
+            temp = head;
+            
+        }else{
+            temp->next = newNode;  // Link the previous node to the new node
+            temp = newNode;   
+        }
+        temp->next = NULL;
+
+         printf("Do you want to insert a new node? y/n:\n");
+        
+        
+        scanf(" %c", &ch);
+
+
+    }  while (ch == 'y' || ch =='Y');
+    
+}
+
+int main(){
+    
+    createNode();
+    return 0;
+}
