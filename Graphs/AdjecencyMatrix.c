08db7fcd122e8792e2c4703afafe6c29c51e5d32
rudra <rudrakhandelwal1212@gmail.com>
2024-11-12 06:52:26 +0530

    add delete in adj matrix

--- Graphs/AdjecencyMatrix.c
+++ Graphs/AdjecencyMatrix.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 
-#define N 4  // Number of nodes
+#define MAX_NODES 100
 
-void printMatrix(int matrix[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", matrix[i][j]);
+int adjMatrix[MAX_NODES][MAX_NODES];
+int numNodes = 0;
+
+void addNode() {
+    numNodes++;
+    printf("Node %d added.\n", numNodes);
+}
+
+void addEdge(int node1, int node2) {
+    adjMatrix[node1][node2] = 1;
+    adjMatrix[node2][node1] = 1;
+    printf("Edge added between Node %d and Node %d.\n", node1, node2);
+}
+
+void deleteNode(int node) {
+    for (int i = node; i < numNodes; i++) {
+        for (int j = 0; j < numNodes; j++) {
+            adjMatrix[i][j] = adjMatrix[i + 1][j]; // Shift rows up
+            adjMatrix[j][i] = adjMatrix[j][i + 1]; // Shift columns left
+        }
+    }
+    numNodes--;
+    printf("Node %d deleted.\n", node);
+}
+
+void displayGraph() {
+    printf("Adjacency Matrix:\n");
+    for (int i = 1; i <= numNodes; i++) {
+        for (int j = 1; j <= numNodes; j++) {
+            printf("%d ", adjMatrix[i][j]);
         }
         printf("\n");
     }
 }
 
 int main() {
-    int adjMatrix[N][N] = {0};  // Initialize a 4x4 matrix with 0s
+    addNode(); // Node 1
+    addNode(); // Node 2
+    addNode(); // Node 3
 
-    // Adding edges
-    adjMatrix[0][1] = 1;
-    adjMatrix[1][0] = 1;  // For undirected graph, mark both [i][j] and [j][i]
-    adjMatrix[1][2] = 1;
-    adjMatrix[2][1] = 1;
-    adjMatrix[2][3] = 1;
-    adjMatrix[3][2] = 1;
+    addEdge(1, 2);
+    addEdge(2, 3);
+    addEdge(1, 3);
 
-    printf("Adjacency Matrix:\n");
-    printMatrix(adjMatrix);
+    printf("\nGraph before deleting a node:\n");
+    displayGraph();
+
+    deleteNode(2);
+
+    printf("\nGraph after deleting node 2:\n");
+    displayGraph();
 
     return 0;
 }
