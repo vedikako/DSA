8c5cbfdc85b5d8444175d4cdefdbd51035b05bc1
vedikako <vedika.kodgire12@gmail.com>
2024-12-05 00:59:26 +0530

    Update BubbleSort.c

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
-void bubblesort(int arr[], int n){
-    int temp, flag;
-    for(int i=0; i<n-1; i++){
-        flag = 0;
-        for(int j = 0; j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-                flag = 1;
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        if(flag==0)
-            break;
     }
-    
 }
 
-int main()
-{
-    int arr[] = {10,9,8,7,6,5,4,3,2,1}, n = 10, i;
-    bubblesort(arr,n);
-    
-    for(i=0;i<n-1;i++){
-        printf("%d \n", arr[i]);
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
 
     return 0;
 }
